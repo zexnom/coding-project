5997ed2829c67793d8d41b6a60d983bd00a836c3
zexnom <b022110126@student.utem.edu.my>
2023-03-27 15:36:25 +0800

    exercise 1

---
+++ Script1.js
@@ -0,0 +1,27 @@
+// JavaScript source code
+let data1 = 1
+let data2 = "1"
+console.log(data1, typeof data1)
+console.log(data2, typeof data2)
+
+let data3 = data1 + data2
+console.log(data3, typeof data3)
+
+//Array
+let data = ["one", 2 , true]
+console.log(data[2], typeof data[2])
+
+//object
+let user ={
+    name: "soo",
+    faculty: "FKEKK",
+    phone:{ 
+        office:"011203",
+        mobile: "4911"
+    },
+    emil: "sdandsb@gmail.com",
+}
+console.log(user.faculty)
+console.log(user.phone.mobile)
+console.log(user.phone.office)
+console.log(user.phone.home)
